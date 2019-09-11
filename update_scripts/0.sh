6b6da153896163519661f5dfa0bb21e9059b8f9c
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-11 19:10:13 -0400

    Added ./files and DEMON backups area

--- update_scripts/0.sh
+++ update_scripts/0.sh
@@ -10,6 +10,8 @@ DUT_YLW="\033[1;33m"
 DUT_RED="\033[1;31m"
 DUT_SCRIPT=/var/demon/updater/code/Demon-Update-Tool/update_scripts/$(basename "$0")
 DUT_SCRIPT_CHECKSUM=$(md5sum $DUT_SCRIPT|awk '{print $1}')
+DUT_FILES=/var/demon/updater/code/Demon-Update-Tool/files
+DEMON_BACKUPS=/usr/share/demon/backups/
 
 if [[ "$1" != "$DUT_SCRIPT_CHECKSUM" ]] # ensure this file is not called alone, ity must be called with it's own checksum
   then
@@ -22,9 +24,20 @@ log() {
 export -f log
 log "Hello. I have initialized"
 ### Place all custom update code below this line.
-# 1. fix the font in LightDM:
+#============================================================#
+## 1. fix the font in LightDM:
 log "Fixing the font issue with LightDM"
 find /usr/share/fonts -iname '*.ttf' -type f -exec sudo chmod -v 644 {} \; 2>&1>/dev/null
 find /usr/share/fonts -iname '*.otf' -type f -exec sudo chmod -v 644 {} \; 2>&1>/dev/null
 sudo fc-cache -r -v 2>&1>/dev/null
-# 2. ...
+## 2. Add the RF diretcory to the Menu:
+log "Adding RF menu category"
+if [ ! -d ${DEMON_BACKUPS}/menus ] # backups directory that I forgot to make in ISO
+  then
+      mkdir -p /usr/share/demon/backups/menus
+fi
+cp ${DUT_FILES}/desktop/rf.directory /usr/share/desktop-directories/
+log "Backing up XFCE4 Menu to ${DEMON_BACKUPS}/menus/xfce-applications.menu-$(date +%s)"
+cp /etc/xdg/menus/xfce-applications.menu ${DEMON_BACKUPS}/menus/xfce-applications.menu-$(date +%s)
+cp ${DUT_FILES}/menus/xfce-applications.menu /etc/xdg/menus/
+## 3. ?
