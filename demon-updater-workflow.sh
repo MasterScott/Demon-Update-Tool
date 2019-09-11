2c28a175dd1a2e2a835f3ecc5e3543b24dfb2afb
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-11 12:07:17 -0400

    Getters and Setters, separate us all ...

--- demon-updater-workflow.sh
+++ demon-updater-workflow.sh
@@ -17,14 +17,15 @@ DUT_RED="\033[1;31m]"
 log() { # pass to me the phrase to be logged only as a string
   printf "${DUT_GRN}[${DUT_CYN}$(basename "$0")${DUT_YLW} log${DUT_GRN}]${DUT_RST}: $1 \n"
 }
-updateVersion() { # pass to me the version to log the update
+setCurrentLevel() { # pass to me the version to log the update
+  log "Updating $(getCurrentLevel) to level $1 in /etc/demon/version"
   echo $1 > /etc/demon/version
 }
 getCurrentLevel() {
   echo $(cat /etc/demon/version)
 }
 export -f log
-export -f updateVersion
+export -f setCurrentLevel
 export -f getCurrentLevel
 
 if [[ "$1" != "$DUT_SCRIPT_CHECKSUM" ]] # ensure this file is not called alone, ity must be called with it's own checksum
@@ -35,15 +36,15 @@ else
   log "Initializing update tool"
   log "Current version: $DUT_CURRENT_LEVEL"
   log "Highest level available: $DUT_HIGHEST_LEVEL"
-  DUT_NEXT_LEVEL=$((DUT_CURRENT_LEVEL+=1))
   if [[ $DUT_CURRENT_LEVEL -lt $DUT_HIGHEST_LEVEL ]]
     then # System requires an update!
+      DUT_NEXT_LEVEL=$((DUT_CURRENT_LEVEL+=1))
       log "Demon requires update."
       while [ $DUT_NEXT_LEVEL -le $DUT_HIGHEST_LEVEL ]
         do # run the script:
           log "Running update level: $DUT_NEXT_LEVEL"
           ${DUT_LEVELS}/${DUT_NEXT_LEVEL}.sh
-          updateVersion $DUT_NEXT_LEVEL # record that we (at least tried) applied the update ...
+          setCurrentLevel $DUT_NEXT_LEVEL # record that we (at least tried) applied the update ...
           DUT_NEXT_LEVEL=$((DUT_NEXT_LEVEL+=1)) # postfix and run again ...
       done
       log "${DUT_GRN}System at version $(getCurrentLevel). All updates are complete."
