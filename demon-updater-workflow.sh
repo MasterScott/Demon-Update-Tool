22d857ab12a9b27bc593e1f7ee40af1a793e03c8
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-11 12:00:49 -0400

    Extracted color items

--- demon-updater-workflow.sh
+++ demon-updater-workflow.sh
@@ -8,9 +8,13 @@ DUT_LEVELS=/var/demon/updater/code/Demon-Update-Tool/update_scripts/
 DUT_SCRIPT_CHECKSUM=$(md5sum $DUT_SCRIPT|awk '{print $1}') # This will generate an md5
 DUT_CURRENT_LEVEL=$2 # current version from /etc/demon/version
 DUT_HIGHEST_LEVEL=$(ls $DUT_LEVELS|sort -u|tail -n 1|awk -F. '{print $1}')
+DUT_GRN="\033[1;32m"
+DUT_RST="\033[0m"
+DUT_CYN="\033[1;36m"
+DUT_YLW="\033[1;33m"
 
 log() { # pass to me the phrase to be logged only as a string
-  printf "\033[1;32m[\033[1;36m$(basename "$0")\033[1;33m log\033[1;32m]\033[0m: $1 \n"
+  printf "${DUT_GRN}[${DUT_CYN}$(basename "$0")${DUT_YLW} log${DUT_GRN}]${DUT_RST}: $1 \n"
 }
 updateVersion() { # pass to me the version to log the update
   echo $1 > /etc/demon/version
@@ -31,12 +35,18 @@ else
   log "Current version: $DUT_CURRENT_LEVEL"
   log "Highest level available: $DUT_HIGHEST_LEVEL"
   DUT_NEXT_LEVEL=$((DUT_CURRENT_LEVEL+=1))
-  while [ $DUT_NEXT_LEVEL -le $DUT_HIGHEST_LEVEL ]
-    do # run the script:
-      log "Running update level: $DUT_NEXT_LEVEL"
-      ${DUT_LEVELS}/${DUT_NEXT_LEVEL}.sh
-      updateVersion $DUT_NEXT_LEVEL # record that we (at least tried) applied the update ...
-      DUT_NEXT_LEVEL=$((DUT_NEXT_LEVEL+=1)) # postfix and run again ...
-  done
+  if [ $DUT_CURRENT_LEVEL -lt $DUT_HIGHEST_LEVEL ]
+    then # System requires an update!
+      log "Demon requires update."
+      while [ $DUT_NEXT_LEVEL -le $DUT_HIGHEST_LEVEL ]
+        do # run the script:
+          log "Running update level: $DUT_NEXT_LEVEL"
+          ${DUT_LEVELS}/${DUT_NEXT_LEVEL}.sh
+          updateVersion $DUT_NEXT_LEVEL # record that we (at least tried) applied the update ...
+          DUT_NEXT_LEVEL=$((DUT_NEXT_LEVEL+=1)) # postfix and run again ...
+      done
+      log "${DUT_GRN}System at version $(getCurrentLevel). All updates are complete."
+  else
+    log "${DUT_GRN}System is already up to date. Version $(getCurrentLevel)${DUT_RST}"
+  fi # done
 fi
-log "System now at version \033[1;32m$(getCurrentLevel)\033[0m. All updates are complete."
