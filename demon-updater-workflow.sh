1dbbf749162d213e486b56660d34a02bc2a94b7a
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-11 09:58:11 -0400

    Added first 0.sh, WIP, new init ver -1

--- demon-updater-workflow.sh
+++ demon-updater-workflow.sh
@@ -2,19 +2,23 @@
 # This is the workflow of the updater It will call scripts based on the version of Demon Linux
 # 2019 - WeakNet Labs - Douglas Berdeaux, DemonLinux.com ^vv^
 #
-#SCRIPT=/usr/local/sbin/$(basename "$0") # this will be the full path to this script.
-SCRIPT=$(basename "$0")
-SCRIPT_CHECKSUM=$(md5sum $SCRIPT|awk '{print $1}') # This will generate an md5
+DUT_SCRIPT=$(basename "$0") # the name of this script
+DUT_SCRIPT_CHECKSUM=$(md5sum $SCRIPT|awk '{print $1}') # This will generate an md5
 source ~/.bashrc
+DUT_CURRENT_LEVEL=$2 # current version from /etc/demon/version
 
-log() {
+log() { # pass to me the phrase to be logged only as a string
   printf "[log]: $1 \n"
 }
+updateVersion() { # pass to me the version to log the update
+  echo $1 > /etc/demon/version
+}
 export -f log
+export -f updateVersion
 
-if [[ "$1" != "$SCRIPT_CHECKSUM" ]] # ensure this file is not called alone, ity must be called with it's own checksum
+if [[ "$1" != "$DUT_SCRIPT_CHECKSUM" ]] # ensure this file is not called alone, ity must be called with it's own checksum
   then
-    log "Got SCRIPT_CHECKSUM: $SCRIPT_CHECKSUM"
+    log "Got SCRIPT_CHECKSUM: $DUT_SCRIPT_CHECKSUM"
     printf "\n[ERROR]: This script should not be called directly.\n\tPlease use the \"demon-updater.sh\" command\n\n"
     exit 1337
 else
--- demon-updater.sh
+++ demon-updater.sh
@@ -4,12 +4,12 @@
 #  This tool is self-maintained using GitHUB repository versioning.
 #  2019 - WeakNet Labs, Douglas Berdeaux, DemonLinux.com
 #
-WORKFLOW_SCRIPT=/usr/local/sbin/demon-updater-workflow.sh
-WORKFLOW_SCRIPT_CHECKSUM=$(md5sum $WORKFLOW_SCRIPT|awk '{print $1}')
-CODE_DIR=/var/demon/updater/code
-REPO_DIR=$CODE_DIR/Demon-Update-Tool
-GIT_REPO_URL=https://github.com/weaknetlabs/Demon-Update-Tool
-CURRENT_LEVEL=$(cat /etc/demon/version||echo 0)
+DUT_WORKFLOW_SCRIPT=/usr/local/sbin/demon-updater-workflow.sh
+DUT_WORKFLOW_SCRIPT_CHECKSUM=$(md5sum $WORKFLOW_SCRIPT|awk '{print $1}')
+DUT_CODE_DIR=/var/demon/updater/code
+DUT_REPO_DIR=$CODE_DIR/Demon-Update-Tool
+DUT_GIT_REPO_URL=https://github.com/weaknetlabs/Demon-Update-Tool
+DUT_CURRENT_LEVEL=$(cat /etc/demon/version||echo -1)
 
 printf "\033[1;31m\nDemon Linux Update Tool\n2019 WeakNet Labs, Douglas Berdeaux\n\n\033[0m"
 source ~/.bashrc
@@ -18,7 +18,7 @@ log() {
 }
 export -f log
 
-log "Current version $CURRENT_LEVEL"
+log "Current version $DUT_CURRENT_LEVEL"
 ### Check if Demon Linux directory structure exists:
 if [[ ! -d "/etc/demon/" ]]
   then
@@ -26,24 +26,24 @@ if [[ ! -d "/etc/demon/" ]]
     mkdir -p /etc/demon
     echo 0 > /etc/demon/version # init as version 0
 fi
-if [[ ! -d "$CODE_DIR" ]]
+if [[ ! -d "$DUT_CODE_DIR" ]]
   then
     log "Creating $CODE_DIR local directory structure"
-    mkdir -p $CODE_DIR
+    mkdir -p $DUT_CODE_DIR
     log "Cloning remote repository"
-    cd $CODE_DIR
-    git clone $GIT_REPO_URL
+    cd $DUT_CODE_DIR
+    git clone $DUT_GIT_REPO_URL
 else
-  cd $REPO_DIR && git pull
+  cd $DUT_REPO_DIR && git pull
 fi
 
 log "Copying binary to \$PATH"
-cp $REPO_DIR/*.sh /usr/local/sbin # clobber the old files if necessary
+cp $DUT_REPO_DIR/*.sh /usr/local/sbin # clobber the old files if necessary
 chmod +x /usr/local/sbin/demon-updater-workflow.sh
 chmod +x /usr/local/sbin/demon-updater.sh
 log "Tool up to date, proceed with updates"
 
 ### Calling updater-workflow script
-log "Using CHECKSUM: $WORKFLOW_SCRIPT_CHECKSUM"
-log "Calling WORKFLOW_SCRIPT: $WORKFLOW_SCRIPT"
-$WORKFLOW_SCRIPT $WORKFLOW_SCRIPT_CHECKSUM
+log "Using CHECKSUM: $DUT_WORKFLOW_SCRIPT_CHECKSUM"
+log "Calling WORKFLOW_SCRIPT: $DUT_WORKFLOW_SCRIPT"
+$DUT_WORKFLOW_SCRIPT $DUT_WORKFLOW_SCRIPT_CHECKSUM $DUT_CURRENT_LEVEL
