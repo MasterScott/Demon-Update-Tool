d8e0d4269c9f1bac05061d86a2e040e16ee6c12b
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-11 10:26:04 -0400

    Added 1.sh for testing

---
+++ update_scripts/1.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+source ~/.bashrc
+log() {
+  printf "\033[1;32m[\033[1;33m$(basename $0)-log\033[1;32m]\033[0m: $1 \n"
+}
+export -f log
+
+log " <-- This update script has initialized"
