78254638ab18eb3636c539f199d2e7ee194f4aa8
Douglas Berdeaux <weaknetlabs@gmail.com>
2019-09-12 10:53:29 -0400

    Update demon-updater-workflow.sh

--- demon-updater-workflow.sh
+++ demon-updater-workflow.sh
@@ -4,30 +4,34 @@
 #
 source ~/.bashrc
 #DUT_SCRIPT=/usr/local/sbin/$(basename "$0") # the name of this script
-DUT_SCRIPT=/var/demon/updater/code/Demon-Update-Tool/demon-updater-workflow.sh
-DUT_LEVELS=/var/demon/updater/code/Demon-Update-Tool/update_scripts/
-DUT_SCRIPT_CHECKSUM=$(md5sum $DUT_SCRIPT|awk '{print $1}') # This will generate an md5
-DUT_HIGHEST_LEVEL=$(ls $DUT_LEVELS|sort -u|tail -n 1|awk -F. '{print $1}')
-DUT_GRN="\033[1;32m"
-DUT_RST="\033[0m"
-DUT_CYN="\033[1;36m"
-DUT_YLW="\033[1;33m"
-DUT_RED="\033[1;31m"
+export DUT_SCRIPT=/var/demon/updater/code/Demon-Update-Tool/demon-updater-workflow.sh
+export DUT_LEVELS=/var/demon/updater/code/Demon-Update-Tool/update_scripts/
+export DUT_SCRIPT_CHECKSUM=$(md5sum $DUT_SCRIPT|awk '{print $1}') # This will generate an md5
+export DUT_HIGHEST_LEVEL=$(ls $DUT_LEVELS|sort -u|tail -n 1|awk -F. '{print $1}')
+export DUT_GRN="\033[1;32m"
+export DUT_RST="\033[0m"
+export DUT_CYN="\033[1;36m"
+export DUT_YLW="\033[1;33m"
+export DUT_RED="\033[1;31m"
+export DUT_NOTIFY_APP="Demon Update Tool Notification"
 
-log() { # pass to me the phrase to be logged only as a string
+log () { # pass to me the phrase to be logged only as a string
   printf "${DUT_GRN}[${DUT_CYN}$(basename "$0")${DUT_YLW} log${DUT_GRN}]${DUT_RST}: $1 \n"
 }
-setCurrentLevel() { # pass to me the version to log the update
+setCurrentLevel () { # pass to me the version to log the update
   log "Updating $(getCurrentLevel) to level $1 in /etc/demon/version"
   echo $1 > /etc/demon/version
 }
-
-getCurrentLevel() {
+notify () {
+  notify-send "$1" "$2"
+}
+getCurrentLevel () {
   echo $(cat /etc/demon/version)
 }
 export -f log
 export -f setCurrentLevel
 export -f getCurrentLevel
+export -f notify
 
 if [[ "$1" != "$DUT_SCRIPT_CHECKSUM" ]] # ensure this file is not called alone, ity must be called with it's own checksum
   then
@@ -59,6 +63,7 @@ else
           fi
       done
       log "${DUT_GRN}System at version $(getCurrentLevel). All updates are complete.${DUT_RST}"
+      notify $DUT_NOTIFY_APP "The Demon is now up to date."
   else
     log "${DUT_GRN}System is already up to date. Version $(getCurrentLevel)${DUT_RST}"
   fi # done
